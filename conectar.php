96376b814f535db4a63e4e3ddd54082688666337
Marco Ramirez <marco.ramirez@mojix.com>
2016-03-21 23:10:26 -0400

    first commit of develop branch

--- conectar.php
+++ conectar.php
@@ -1,7 +1,7 @@
 <?php
 
   include("config.php"); 
-  $conexion=mysql_connect($Servidor,$Usuario,$Password) or die("Error: El servidor no puede conectar con la base de datos");
-  $descriptor=mysql_select_db($BaseDeDatos,$conexion);
+  $conexion=mysqli_connect($Servidor,$Usuario,$Password) or die("Error: El servidor no puede conectar con la base de datos");
+  $descriptor=mysqli_select_db($BaseDeDatos,$conexion);
 
 ?>
